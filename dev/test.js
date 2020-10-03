03b5394ab854d6b6d936916e60acbe3782e80c57
Brandon <brandon.feinstein@hotmail.com>
2020-10-03 13:07:17 -0700

    First Commit

---
+++ dev/test.js
@@ -0,0 +1,26 @@
+const Blockchain = require("./blockchain");
+
+const bitcoin = new Blockchain();
+
+// const previousBlockHash = "DKLFSJLK232323";
+// const currentBlockData = [
+//   {
+//     amount: 10,
+//     sender: "DJAKFLSDJAL232",
+//     recipient: "DAJFLDAJFLS323E423432",
+//   },
+//   {
+//     amount: 30,
+//     sender: "DAFDANHMHJHGJG",
+//     recipient: "FGHGFFH45464",
+//   },
+//   {
+//     amount: 200,
+//     sender: "XBVCBGF4546",
+//     recipient: "VNBVCBVNTR45645",
+//   },
+// ];
+
+// console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData)); //26996
+// console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 26996));
+console.log(bitcoin);
